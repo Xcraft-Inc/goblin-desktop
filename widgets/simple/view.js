fe4303d70751fdb51b3693274fa47bf491473e74
Samuel Loup <loup@epsitec.ch>
2018-01-12 14:14:56 +0100

    WIP: ETL

---
+++ widgets/simple/view.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import importer from 'laboratory/importer';
+import Widget from 'laboratory/widget';
+import View from 'laboratory/view';
+import Container from 'gadgets/container/widget';
+
+const widgetImporter = importer ('widget');
+
+class SimpleView extends View {
+  render () {
+    const {workitemId} = this.props;
+    if (!workitemId) {
+      return null;
+    }
+
+    const workitem = workitemId.split ('@')[0];
+    const wireWidget = Widget.Wired (widgetImporter (workitem));
+    const WiredWidget = wireWidget (workitemId);
+    return (
+      <Container kind="views">
+        <WiredWidget />
+      </Container>
+    );
+  }
+}
+
+export default SimpleView;
