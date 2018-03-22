b9ea7aec0bf78c5569e7da69417f874214702a90
Samuel Loup <loup@epsitec.ch>
2018-03-23 00:25:17 +0100

    Wizard widget is now able to render as a dialog

--- widgets/wizard/widget.js
+++ widgets/wizard/widget.js
@@ -5,11 +5,15 @@ import Label from 'gadgets/label/widget';
 import Button from 'gadgets/button/widget';
 import importer from 'laboratory/importer/';
 
+import DialogModal from 'gadgets/dialog-modal/widget';
+import Separator from 'gadgets/separator/widget';
+
 const uiImporter = importer('ui');
 class Wizard extends Form {
   constructor() {
     super(...arguments);
     this.onNext = this.onNext.bind(this);
+    this.onCancel = this.onCancel.bind(this);
     this.setForm = this.setForm.bind(this);
     this.doProxy = this.doProxy.bind(this);
   }
@@ -29,6 +33,11 @@ class Wizard extends Form {
     this.doAs(service, 'next');
   }
 
+  onCancel() {
+    const service = this.props.id.split('@')[0];
+    this.doAs(service, 'cancel');
+  }
+
   doProxy(action, args) {
     const workitem = this.props.id.split('@')[0];
     this.doAs(workitem, action, args);
@@ -43,7 +52,7 @@ class Wizard extends Form {
   }
 
   render() {
-    const {id, title} = this.props;
+    const {id, title, kind} = this.props;
     if (!id) {
       return null;
     }
@@ -60,37 +69,80 @@ class Wizard extends Form {
     }
 
     const Form = this.Form;
-    return (
-      <Container kind="view" width="800px" spacing="large">
-        <Container kind="pane-header">
-          <Label text={title} kind="pane-header" />
-        </Container>
-        <Container kind="panes">
-          <Container kind="pane">
-            <Form {...this.formConfig}>
-              <Step
-                {...this.props}
-                theme={this.context.theme}
-                do={this.doProxy}
-                setForm={this.setForm}
-              />
-            </Form>
+
+    if (kind === 'dialog') {
+      return (
+        <DialogModal width="1500px" zIndex="12">
+          <Label text={title} />
+          <Separator kind="space" height="10px" />
+          <Container kind="panes">
+            <Container kind="pane">
+              <Form {...this.formConfig}>
+                <Step
+                  {...this.props}
+                  theme={this.context.theme}
+                  do={this.doProxy}
+                  setForm={this.setForm}
+                />
+              </Form>
+            </Container>
+          </Container>
+          <Separator kind="space" height="20px" />
+          <Container kind="row">
+            <Button
+              busy={this.props.busy}
+              width="0px"
+              grow="1"
+              kind="action"
+              place="1/2"
+              glyph="solid/step-forward"
+              text="Suivant"
+              onClick={this.onNext}
+            />
+            <Button
+              glyph="solid/times"
+              text="Annuler"
+              grow="1"
+              kind="action"
+              place="2/2"
+              onClick={this.onCancel}
+            />
+          </Container>
+        </DialogModal>
+      );
+    } else {
+      return (
+        <Container kind="view" width="800px" spacing="large">
+          <Container kind="pane-header">
+            <Label text={title} kind="pane-header" />
+          </Container>
+          <Container kind="panes">
+            <Container kind="pane">
+              <Form {...this.formConfig}>
+                <Step
+                  {...this.props}
+                  theme={this.context.theme}
+                  do={this.doProxy}
+                  setForm={this.setForm}
+                />
+              </Form>
+            </Container>
+          </Container>
+          <Container kind="actions">
+            <Button
+              busy={this.props.busy}
+              width="0px"
+              grow="1"
+              kind="action"
+              place="1/1"
+              glyph="solid/step-forward"
+              text="Suivant"
+              onClick={this.onNext}
+            />
           </Container>
         </Container>
-        <Container kind="actions">
-          <Button
-            busy={this.props.busy}
-            width="0px"
-            grow="1"
-            kind="action"
-            place="1/1"
-            glyph="solid/step-forward"
-            text="Suivant"
-            onClick={this.onNext}
-          />
-        </Container>
-      </Container>
-    );
+      );
+    }
   }
 }
 
