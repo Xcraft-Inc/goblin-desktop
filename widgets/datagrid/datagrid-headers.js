db721f30d2bc70b89193ed253cf471a5496d06ce
Jonny Quarta <quarta@epsitec.ch>
2018-10-31 16:42:31 +0100

    Add header sorting fields in datagrid headers

--- widgets/datagrid/datagrid-headers.js
+++ widgets/datagrid/datagrid-headers.js
@@ -3,7 +3,100 @@ import Widget from 'laboratory/widget';
 import Form from 'laboratory/form';
 import Container from 'gadgets/container/widget';
 import Connect from 'laboratory/connect';
+import Label from 'gadgets/label/widget';
 import _ from 'lodash';
+
+class Header extends Widget {
+  constructor() {
+    super(...arguments);
+    this.tooltips = {
+      asc: {
+        id: 'ascending order',
+      },
+      desc: {
+        id: 'descending order',
+      },
+    };
+  }
+
+  render() {
+    const {
+      id,
+      index,
+      column,
+      datagrid,
+      doAsDatagrid,
+      component,
+      headerCell,
+      context,
+    } = this.props;
+    const self = this;
+
+    function renderSortingHeader() {
+      return (
+        <Connect
+          glyph={() => {
+            const key = datagrid.getModelValue(`.sort.key`);
+            const dir = datagrid.getModelValue(`.sort.dir`);
+
+            if (key === column.get('field')) {
+              return dir === 'asc' ? 'solid/arrow-up' : 'solid/arrow-down';
+            } else {
+              return 'solid/circle';
+            }
+          }}
+          tooltip={() => {
+            const key = datagrid.getModelValue(`.sort.key`);
+            const dir = datagrid.getModelValue(`.sort.dir`);
+
+            if (key === column.get('field')) {
+              return dir === 'asc' ? self.tooltips.asc : self.tooltips.desc;
+            } else {
+              return null;
+            }
+          }}
+        >
+          <Label
+            onClick={() =>
+              doAsDatagrid('toggle-sort', {
+                field: column.get('field'),
+              })
+            }
+            spacing="overlap"
+          />
+        </Connect>
+      );
+    }
+
+    function renderComponent() {
+      const CellUI = component.WithState(headerCell, 'id')('.id');
+
+      return (
+        <CellUI
+          key={`${id}_${index}`}
+          id={id}
+          index={index}
+          theme={context.theme}
+          column={column}
+          datagrid={datagrid}
+          doAsDatagrid={doAsDatagrid}
+          contextId={context.contextId}
+        />
+      );
+    }
+
+    if (column.get('sortable')) {
+      return (
+        <div style={{display: 'flex'}}>
+          {renderComponent()}
+          {renderSortingHeader()}
+        </div>
+      );
+    } else {
+      return renderComponent();
+    }
+  }
+}
 
 class DataGridHeaders extends Form {
   constructor() {
@@ -31,24 +124,23 @@ class DataGridHeaders extends Form {
 
     function renderCell(index) {
       if (entityUI && entityUI.headerCell) {
-        const CellUI = self.WithState(entityUI.headerCell, 'id')('.id');
-
         return (
           <Connect
             key={`${id}_${index}`}
             column={() => datagrid.getModelValue(`.columns[${index}]`)}
           >
-            <CellUI
+            <Header
               key={`${id}_${index}`}
               id={id}
               index={index}
-              theme={self.context.theme}
               datagrid={datagrid}
               doAsDatagrid={(quest, args) => {
                 const service = datagrid.props.id.split('@')[0];
                 self.doAs(service, quest, args);
               }}
-              contextId={self.context.contextId}
+              component={self}
+              headerCell={entityUI.headerCell}
+              context={self.context}
             />
           </Connect>
         );
