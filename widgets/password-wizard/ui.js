92a4a3a07823165a99769ccfa6ecf9a726eecd41
Gillian Fries <fries@epsitec.ch>
2019-02-13 14:25:07 +0100

    Add password wizard in goblin desktop

---
+++ widgets/password-wizard/ui.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Container from 'gadgets/container/widget';
+import Field from 'gadgets/field/widget';
+import Button from 'gadgets/button/widget';
+
+function renderMain(props) {
+  const type = props.showPassword === 'true' ? {} : {type: 'password'};
+  return (
+    <Container kind="column" grow="1">
+      <Field
+        kind="field"
+        labelWidth="30px"
+        labelGlyph="solid/lock"
+        hintText="Mot de passe"
+        model=".form.password"
+        grow="1"
+        {...type}
+      />
+      <Field
+        kind="bool"
+        labelText="Afficher le mot de passe"
+        model=".form.showPassword"
+      />
+      <Button
+        kind="action"
+        grow="1"
+        place="1/1"
+        width="300px"
+        glyph="solid/user-secret"
+        text="Changer le mot de passe"
+        onClick={() =>
+          props.do('createRandomPassword', {
+            passwordLength: props.passwordLength,
+          })
+        }
+      />
+    </Container>
+  );
+}
+
+/******************************************************************************/
+
+const mapProps = entity => {
+  return {
+    showPassword: entity.get('form.showPassword'),
+    passwordLength: entity.get('form.passwordLength'),
+  };
+};
+
+/******************************************************************************/
+export default {
+  mappers: {
+    main: mapProps,
+  },
+
+  main: renderMain,
+};
