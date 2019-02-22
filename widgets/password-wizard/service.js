05b201ce530c5948ea4e8c88130312b5d0cab5fc
Yannick Vessaz <vessaz@epsitec.ch>
2019-02-22 15:26:24 +0100

    Use crypto.randomPassword from xcraft-core-utils

--- widgets/password-wizard/service.js
+++ widgets/password-wizard/service.js
@@ -1,6 +1,6 @@
 'use strict';
 const {buildWizard} = require('goblin-desktop');
-const createRandomPassword = require('./createRandomPassword');
+const crypto = require('xcraft-core-utils/lib/crypto.js');
 
 const config = {
   name: 'password',
@@ -9,7 +9,7 @@ const config = {
     setRandomPassword: function*(quest) {
       const state = quest.goblin.getState();
       const passwordLength = state.get('form.passwordLength');
-      const password = createRandomPassword(passwordLength);
+      const password = crypto.randomPassword(passwordLength);
       yield quest.me.change({
         path: 'form.password',
         newValue: password,
