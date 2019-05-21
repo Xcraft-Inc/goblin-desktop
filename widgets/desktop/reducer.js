4f3d9f234779def850edda77dc253726bc39869a
Mathieu Schroeter <mathieu@schroetersa.ch>
2019-05-21 10:43:58 +0200

    Add a cache for widgets in the session (desktop)
    This cache is limited in size. An entry in the cache can be lost at any
    time.
    
    See comments in the source-code for details.

--- widgets/desktop/reducer.js
+++ widgets/desktop/reducer.js
@@ -1,13 +1,34 @@
 //T:2019-02-27
 import {fromJS} from 'immutable';
+
 const initialState = fromJS({
   showPrompt: false,
+  widgetsCache: {},
 });
+
+const WIDGETS_CACHE_LIMIT = 128;
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case 'TOGGLEPROMPT': {
       return state.set('showPrompt', !state.get('showPrompt'));
+    }
+
+    /* Limited cache in size to store values for some widgets like for example
+     * the ScrollableContainer. Because this cache is limited, it's possible
+     * to lose the data at any time.
+     */
+    case 'WIDGET_CACHE': {
+      const id = action.widgetId;
+      if (state.has(`widgetsCache.${id}`)) {
+        return state.set(`widgetsCache.${id}`, action.value);
+      }
+      let widgetsCache = state.get('widgetsCache');
+      if (widgetsCache.size >= WIDGETS_CACHE_LIMIT) {
+        const _id = widgetsCache.keySeq().first();
+        state = state.del(`widgetsCache.${_id}`);
+      }
+      return state.set(`widgetsCache.${id}`, action.value);
     }
   }
   return state;
