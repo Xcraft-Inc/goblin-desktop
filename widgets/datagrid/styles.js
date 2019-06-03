cbca0245df66db4028181b0eb404a920f6c7f3b3
Mathieu Schroeter <schroeter@epsitec.ch>
2019-06-03 09:03:34 +0200

    Add hover with the datagrid

--- widgets/datagrid/styles.js
+++ widgets/datagrid/styles.js
@@ -1,10 +1,13 @@
 //T:2019-02-27
 
-export default function styles() {
+export default function styles(theme) {
   const entity = {
     flexGrow: 1,
     //marginRight: '20px',
     width: '100%',
+    ':hover': {
+      backgroundColor: theme.palette.tableHoverBackground,
+    },
   };
 
   return {entity};
