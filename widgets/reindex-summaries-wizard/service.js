aa239b463146a32af9eac3167d2afa5e95e9e45d
Samuel Loup <samuel.loup@gmail.com>
2020-01-20 16:30:27 +0100

    Add reindex summaries wizard

---
+++ widgets/reindex-summaries-wizard/service.js
@@ -0,0 +1,103 @@
+'use strict';
+//T:2019-02-27
+
+const T = require('goblin-nabu');
+const {buildWizard} = require('goblin-desktop');
+const workshopConfig = require('xcraft-core-etc')().load('goblin-workshop');
+const entityStorage = workshopConfig.entityStorageProvider.replace(
+  'goblin-',
+  ''
+);
+
+function buildTableList(tableList) {
+  const data = {
+    header: [
+      {
+        name: 'description',
+        grow: '1',
+        textAlign: 'left',
+      },
+    ],
+    rows: [],
+  };
+
+  for (const table of tableList) {
+    data.rows.push({
+      id: table,
+      description: table,
+    });
+  }
+
+  return data;
+}
+
+/******************************************************************************/
+
+const config = {
+  name: 'reindex-summaries',
+  title: T('Réindexeur'),
+  dialog: {
+    width: '500px',
+  },
+  gadgets: {
+    tablesTable: {
+      type: 'table',
+      onActions: {
+        select: (state, action) => {
+          return state.set('form.selectedTables', action.get('selectedIds'));
+        },
+        doubleClick: state => state,
+      },
+    },
+  },
+  steps: {
+    initialize: {
+      quest: function*(quest) {
+        const r = quest.getStorage(entityStorage);
+        const tableList = yield r.listTableFromDb({fromDb: quest.getSession()});
+        yield quest.me.useTablesTable({
+          action: 'setData',
+          payload: {data: buildTableList(tableList)},
+        });
+
+        yield quest.me.next();
+      },
+    },
+    prepare: {
+      updateButtonsMode: 'onChange',
+      buttons: function(quest, buttons, form) {
+        const selectedTables = form.get('selectedTables');
+        const disabled =
+          !selectedTables || (selectedTables && selectedTables.length < 1);
+        return buttons.set('main', {
+          glyph: 'solid/sync',
+          text: 'Démarrer la réindexation',
+          grow: disabled ? '0.5' : '2',
+          disabled: disabled,
+        });
+      },
+      form: {},
+      quest: function*(quest, form) {},
+    },
+    finish: {
+      form: {},
+      quest: function*(quest, form, next) {
+        const desktopId = quest.getDesktop();
+        //const desktop = quest.getAPI(desktopId).noThrow();
+        const workshopAPI = quest.getAPI('workshop');
+        for (const table of form.selectedTables) {
+          yield workshopAPI.reindexEntitiesFromStorage({
+            desktopId,
+            type: table,
+            status: ['draft', 'trashed', 'archived', 'published'],
+            batchSize: 1000,
+          });
+        }
+
+        yield quest.me.next();
+      },
+    },
+  },
+};
+
+module.exports = buildWizard(config);
---
+++ widgets/reindex-summaries-wizard/ui.js
@@ -0,0 +1,32 @@
+//T:2019-02-27
+import React from 'react';
+import T from 't';
+import Container from 'goblin-gadgets/widgets/container/widget';
+import Label from 'goblin-gadgets/widgets/label/widget';
+import Separator from 'goblin-gadgets/widgets/separator/widget';
+import Field from 'goblin-gadgets/widgets/field/widget';
+
+function prepare(props) {
+  return (
+    <Container kind="column" grow="1">
+      <Label text={T('Sélectionnez les entités à réindexer')} />
+      <Separator kind="space" height="10px" />
+      <Container kind="row">
+        <Field
+          kind="gadget"
+          name="tablesTable"
+          selectionMode="multi"
+          frame="true"
+          hasButtons="true"
+          height="300px"
+          grow="1"
+        />
+      </Container>
+    </Container>
+  );
+}
+
+/******************************************************************************/
+export default {
+  prepare,
+};
