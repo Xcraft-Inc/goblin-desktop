7e3fe67dc9c825e2647cbd3582dbd74875ab0d28
Daniel Roux <roux@epsitec.ch>
2020-01-21 14:18:45 +0100

    Westeros: Use Gauge in facet.

--- widgets/facet-filter-button/widget.js
+++ widgets/facet-filter-button/widget.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Widget from 'laboratory/widget';
+import Gauge from 'gadgets/gauge/widget';
+import T from 'nabu/t/widget';
 
 export default class FacetFilterButton extends Widget {
   constructor() {
@@ -7,13 +9,28 @@ export default class FacetFilterButton extends Widget {
   }
 
   render() {
+    let range = null;
+    if (this.props.count !== this.props.total) {
+      range = this.props.count;
+    }
+
+    const value = (this.props.count * 100) / this.props.total;
+
     return (
       <div
         className={this.styles.classNames.facetFilterButton}
         onClick={this.props.onClick}
       >
-        <div className={this.styles.classNames.text}>{this.props.text}</div>
-        <div className={this.styles.classNames.range}>{this.props.range}</div>
+        <T msgid={this.props.text} className={this.styles.classNames.text} />
+        <T msgid={range} className={this.styles.classNames.range} />
+        <Gauge
+          kind="rounded"
+          gradient="red-yellow-green"
+          direction="horizontal"
+          height="12px"
+          width="50px"
+          value={value}
+        />
       </div>
     );
   }
