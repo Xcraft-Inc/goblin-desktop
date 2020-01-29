2fefcee09aa19a79cd0a66cd533ba18d3ba8aeb7
Daniel Roux <roux@epsitec.ch>
2020-01-29 21:39:31 +0000

    Westeros: Add button for create a new facet (don't wiring).

---
+++ widgets/status-filters/styles.js
@@ -0,0 +1,25 @@
+/******************************************************************************/
+
+export default function styles(theme) {
+  const statusFilterAddButton = {
+    'margin': '2px 0px',
+    'padding': '0px 10px',
+    'flexGrow': 1,
+    'display': 'flex',
+    'flexDirection': 'column',
+    'borderRadius': '2px',
+    'border': `1px solid ${theme.palette.textFieldBorderColor}`,
+    'backgroundColor': theme.palette.facetBackground,
+    ':hover': {
+      backgroundColor: theme.palette.facetBackgroundHover,
+    },
+  };
+
+  /******************************************************************************/
+
+  return {
+    statusFilterAddButton,
+  };
+}
+
+/******************************************************************************/
--- widgets/status-filters/widget.js
+++ widgets/status-filters/widget.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Widget from 'goblin-laboratory/widgets/widget';
+import Label from 'goblin-gadgets/widgets/label/widget';
 import FacetFilter from 'goblin-desktop/widgets/facet-filter/widget';
+import * as styles from './styles';
+
+/******************************************************************************/
 
 class StatusFilter extends Widget {
   constructor() {
     super(...arguments);
+    this.styles = styles;
+    this.addFacet = this.addFacet.bind(this);
   }
 
   static get wiring() {
@@ -12,6 +18,23 @@ class StatusFilter extends Widget {
       id: 'id',
       facets: 'facets',
     };
+  }
+
+  addFacet() {
+    // TODO
+  }
+
+  /******************************************************************************/
+
+  renderAdd() {
+    return (
+      <div
+        className={this.styles.classNames.statusFilterAddButton}
+        onClick={this.addFacet}
+      >
+        <Label glyph="solid/plus" />
+      </div>
+    );
   }
 
   render() {
@@ -25,9 +48,12 @@ class StatusFilter extends Widget {
         {Array.from(facets.entries()).map(([k, v], i) => {
           return <FacetFilter id={this.props.id} key={i} name={k} facets={v} />;
         })}
+        {this.renderAdd()}
       </React.Fragment>
     );
   }
 }
+
+/******************************************************************************/
 
 export default Widget.Wired(StatusFilter)();
