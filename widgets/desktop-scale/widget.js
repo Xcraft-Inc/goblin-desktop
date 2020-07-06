21ca63ced1a4ed3021b263c0c4b87bcba5960ac4
Daniel Roux <roux@epsitec.ch>
2020-07-06 10:51:23 +0200

    Westeros: Improve UI of DesktopScale.

--- widgets/desktop-scale/widget.js
+++ widgets/desktop-scale/widget.js
@@ -17,6 +17,7 @@ class DesktopScale extends Widget {
 
     this.state = {
       showDialog: false,
+      zoom: this.props.zoom,
     };
 
     this.min = 0.7;
@@ -30,6 +31,15 @@ class DesktopScale extends Widget {
   set showDialog(value) {
     this.setState({
       showDialog: value,
+    });
+  }
+
+  get zoom() {
+    return this.state.zoom;
+  }
+  set zoom(value) {
+    this.setState({
+      zoom: value,
     });
   }
   //#endregion
@@ -47,8 +57,13 @@ class DesktopScale extends Widget {
   sliderToValue(value) {
     const s = this.min;
     const d = this.max - this.min;
-    const n = s + (value / 100) * d;
-    return Math.floor(n * 10) / 10;
+    return s + (value / 100) * d;
+  }
+
+  changeZoom(zoom) {
+    zoom = Math.round(zoom * 10) / 10;
+    this.doFor(this.context.labId, 'change-zoom', {zoom});
+    this.zoom = zoom;
   }
 
   /******************************************************************************/
@@ -59,57 +74,51 @@ class DesktopScale extends Widget {
     }
 
     const style = {
-      top: `${10 / this.props.zoom}px`,
-      right: `${10 / this.props.zoom}px`,
-      transform: `scale(${1 / this.props.zoom})`,
+      top: `${10 / this.zoom}px`,
+      right: `${10 / this.zoom}px`,
+      transform: `scale(${1 / this.zoom})`,
     };
 
-    const z = `Zoom = ${this.props.zoom}`;
+    const z = `${Math.round(this.zoom * 100)}%`;
+
+    // TODO: Use this mode for Slider!
+    // changeMode="throttled"
+    // throttleDelay={50}
 
     return (
       <div className={this.styles.classNames.dialog} style={style}>
-        <Label width="100px" text={z} />
+        <Label width="50px" glyph="solid/binoculars" glyphSize="200%" />
+        <Label width="70px" text={z} />
         <Button
           shape="rounded"
           glyph="solid/minus"
           tooltip={T('Diminue le zoom')}
-          disabled={this.props.zoom <= this.min}
-          onClick={() => {
-            this.doFor(this.context.labId, 'un-zoom');
-          }}
+          disabled={this.zoom <= this.min}
+          onClick={() => this.changeZoom(this.zoom - 0.1)}
         />
         <Label width="10px" />
         <Slider
           width="150px"
           direction="horizontal"
-          value={this.valueToSlider(this.props.zoom)}
-          changeMode="throttled"
-          throttleDelay={50}
-          onChange={(value) =>
-            this.doFor(this.context.labId, 'change-zoom', {
-              zoom: this.sliderToValue(value),
-            })
-          }
+          value={this.valueToSlider(this.zoom)}
+          changeMode="blur"
+          onChange={(value) => this.changeZoom(this.sliderToValue(value))}
         />
         <Label width="10px" />
         <Button
           shape="rounded"
           glyph="solid/plus"
           tooltip={T('Augmente le zoom')}
-          disabled={this.props.zoom >= this.max}
-          onClick={() => {
-            this.doFor(this.context.labId, 'zoom');
-          }}
+          disabled={this.zoom >= this.max}
+          onClick={() => this.changeZoom(this.zoom + 0.1)}
         />
         <Label width="10px" />
         <Button
           shape="rounded"
-          text="1"
+          text="100%"
           tooltip={T('Remet le zoom 100%')}
-          disabled={this.props.zoom === 1}
-          onClick={() => {
-            this.doFor(this.context.labId, 'default-zoom');
-          }}
+          disabled={this.zoom === 1}
+          onClick={() => this.changeZoom(1)}
         />
         <div className={this.styles.classNames.close}>
           <Button
