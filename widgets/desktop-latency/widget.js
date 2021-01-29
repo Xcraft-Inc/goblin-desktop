7f9b2cd0684f11eb7e63703ea4af3020ecb896af
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-01-29 16:34:11 +0100

    Add a new status button (top-right) for the latency with the hordes

---
+++ widgets/desktop-latency/widget.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Widget from 'goblin-laboratory/widgets/widget';
+import T from 't';
+import Button from 'goblin-gadgets/widgets/button/widget';
+import {ColorManipulator} from 'goblin-theme';
+
+/******************************************************************************/
+
+class DesktopLatencyNC extends Widget {
+  constructor() {
+    super(...arguments);
+
+    this.onChart = this.onChart.bind(this);
+  }
+
+  onChart(horde, latency) {}
+
+  renderLatency(horde, latency) {
+    let color;
+    const _latency = latency.get(0);
+    if (_latency < 100) {
+      color = '#00ff00'; // green
+    } else if (_latency < 200) {
+      color = '#00ffff'; // yellow
+    } else if (_latency < 500) {
+      color = '#ff7f00'; // orange
+    } else {
+      color = '#ff0000'; // red
+    }
+
+    return (
+      <Button
+        key={horde}
+        kind="main-tab-right"
+        glyph="solid/signal"
+        glyphColor={ColorManipulator.darken(color, 0.5)}
+        tooltip={T('Latence avec le serveur {horde} : {latency} ms', '', {
+          horde,
+          latency: _latency,
+        })}
+        onClick={() => this.onChart(horde, latency)}
+      />
+    );
+  }
+
+  render() {
+    const {hordes} = this.props;
+    if (!hordes) {
+      return null;
+    }
+
+    return hordes
+      .entrySeq()
+      .map(([horde, latency]) => this.renderLatency(horde, latency));
+  }
+}
+
+/******************************************************************************/
+
+const DesktopLatency = Widget.connect((state) => {
+  const hordes = state.get(`network.latency`);
+  return {hordes};
+})(DesktopLatencyNC);
+
+export default DesktopLatency;
--- widgets/desktop-topbar/widget.js
+++ widgets/desktop-topbar/widget.js
@@ -8,6 +8,7 @@ import MainTabMenu from 'goblin-desktop/widgets/main-tab-menu/widget';
 import RetroPanel from 'goblin-gadgets/widgets/retro-panel/widget';
 import DesktopThemesMenu from 'goblin-desktop/widgets/desktop-themes-menu/widget';
 import DesktopScale from 'goblin-desktop/widgets/desktop-scale/widget';
+import DesktopLatency from 'goblin-desktop/widgets/desktop-latency/widget';
 import {ColorManipulator} from 'goblin-theme';
 import Contexts from 'goblin-desktop/widgets/contexts/widget.js';
 
@@ -136,6 +137,7 @@ class DesktopTopbarNC extends Widget {
           onClick={this.onTogglePrototypeMode}
         />
         <DesktopScale id={this.props.id} />
+        <DesktopLatency id={this.props.id} />
       </>
     );
   }
