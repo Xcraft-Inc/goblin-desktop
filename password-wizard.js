1562d78cc763d794721b5e02cc08752c68890fec
Mathieu Schroeter <mathieu@schroetersa.ch>
2024-02-27 08:20:41 +0100

    Prevent error when workshop is not available

--- password-wizard.js
+++ password-wizard.js
@@ -1,5 +1,16 @@
 'use strict';
 
+try {
+  require('goblin-workshop');
+} catch (ex) {
+  if (ex.code !== 'MODULE_NOT_FOUND') {
+    throw ex;
+  }
+  console.log(
+    `Skip ${__filename} initialization, goblin-workshop not available`
+  );
+}
+
 /**
  * Retrieve the list of available commands.
  *
