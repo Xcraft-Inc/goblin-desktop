a86dfd011db377217107f580727585c104ae3017
Mathieu Schroeter <mathieu@schroetersa.ch>
2024-02-29 14:36:55 +0100

    Prevent exception

--- quest-run-wizard.js
+++ quest-run-wizard.js
@@ -2,6 +2,18 @@
 
 try {
   require('goblin-workshop');
+
+  /**
+   * Retrieve the list of available commands.
+   *
+   * @returns {Object} The list and definitions of commands.
+   */
+  exports.xcraftCommands = function () {
+    return require(`./widgets/${require('path').basename(
+      __filename,
+      '.js'
+    )}/service.js`);
+  };
 } catch (ex) {
   if (ex.code !== 'MODULE_NOT_FOUND') {
     throw ex;
@@ -10,15 +22,3 @@ try {
     `Skip ${__filename} initialization, goblin-workshop not available`
   );
 }
-
-/**
- * Retrieve the list of available commands.
- *
- * @returns {Object} The list and definitions of commands.
- */
-exports.xcraftCommands = function () {
-  return require(`./widgets/${require('path').basename(
-    __filename,
-    '.js'
-  )}/service.js`);
-};
